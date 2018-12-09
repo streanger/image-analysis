231c930fde6f51146c00fcd8ac959c6738a5d291
streanger <pslisz@o2.pl>
2018-12-09 03:33:45 +0100

    Update remove_simillar_images.py

--- CV2_EXAMPLES/REMOVE_DUPLI_IMG/remove_simillar_images.py
+++ CV2_EXAMPLES/REMOVE_DUPLI_IMG/remove_simillar_images.py
@@ -1,66 +1,85 @@
 #!/usr/bin/python3
+import sys
+import os
+import re
+import shutil
 import numpy as np
 import cv2
-import os
-import sys
-import shutil
 
 def script_path(subpath=""):
-    if subpath:
-        path = os.path.realpath(os.path.dirname(subpath))
-    else:
-        path = os.path.realpath(os.path.dirname(sys.argv[0]))
+    path = os.path.realpath(os.path.dirname(sys.argv[0]))
     os.chdir(path)
     return path
-
-def save_file(file, image):
-    imgDir = "duplicates"
-    if not os.path.exists(imgDir):
-        os.makedirs(imgDir)
-    path = os.path.join(imgDir, file)
-    cv2.imwrite(path, image)
-
-def list_photos():
+    
+def usage():
+    print("-> script for searching for duplicates of image")
+    print("-> duplicates will be found and moved to new dir")
+    print("-> usage example:")
+    print("\tremove_dupli.py    some_image.png")
+    input("-> press enter to exit... ")
+    return True
+    
+def find_threshold_value(filename):
+    value = re.findall("\d+\.\d+", filename)
+    threshold = 0.9
+    try:
+        value = float(value[0])
+        if value > 0 and value < 1:
+            threshold = value
+    except:
+        pass
+    return threshold
+    
+def list_images():
     path = script_path()
     fileTypes = (".png", ".jpeg", ".jpg")
     files = [item for item in os.listdir(path) if item.lower().endswith(fileTypes)]
     return files
-
-def move_file(file, pattern):
-    #read file to image
-    pass
+    
+def move_file(file, currentPath, newPath):
+    currentFilePath = os.path.join(currentPath, file)
+    newFilePath = os.path.join(newPath, file)
+    shutil.move(currentFilePath, newFilePath)
+    return True
+    
+def check_pattern(file, pattern, threshold=0.9):
     img = cv2.imread(file, 0)
     res = cv2.matchTemplate(img, pattern, cv2.TM_CCOEFF_NORMED)
-    threshold = 0.9
     loc = np.where(res >= threshold)
     for pt in zip(*loc[::-1]):
-        #cv2.rectangle(img_gray, pt, (pt[0] + w, pt[1] + h), (255,255,255), 2)
-        print("Pattern found in image:", file)
-        currentFilePath = os.path.join(path, file)
-        newFilePath = os.path.join(newPath, file)
-        shutil.move(currentFilePath, newFilePath)
-        break
+        print("Pattern found in: <{}>".format(file))
+        return True
     else:
-        print("No pattern in image:", file)
-
-
-if __name__ == "__main__":
-    path = script_path()
+        print("No pattern in: <{}>".format(file))
+        return False
+        
+def main(args):
+    currentPath = script_path()
+    
+    # create dir for duplicates
     newDir = "dupli"
-    newPath = os.path.join(path, newDir)
+    newPath = os.path.join(currentPath, newDir)
     if not os.path.exists(newDir):
         os.makedirs(newDir)
-    patternFile = "pattern.png"
-    pattern = cv2.imread(patternFile, 0)
-    w, h = pattern.shape[::-1]
-    files = list_photos()
+        
+    # read pattern to memory
+    try:
+        patternFile = args[0]
+        threshold = find_threshold_value(sys.argv[0])
+        pattern = cv2.imread(patternFile, 0)
+    except:
+        usage()
+        return False
+        
+    # list images & search for duplicates
+    files = list_images()
     for file in files:
-        print(file)
-        move_file(file, pattern)
-
-
-
-
-
-
-
+        status = check_pattern(file, pattern, threshold)
+        if status:
+            move_file(file, currentPath, newPath)
+    return True
+    
+    
+if __name__ == "__main__":
+    main(sys.argv[1:])
+    
