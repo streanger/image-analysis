7d33ffad1090f74947e22e5c81002c9e33c174d3
streanger <pslisz@o2.pl>
2019-01-26 15:13:30 +0100

    Create show_image.py

---
+++ show_image.py
@@ -0,0 +1,18 @@
+import cv2
+
+def show_image(title, image):
+    '''
+    WINDOW_AUTOSIZE
+    WINDOW_FREERATIO
+    WINDOW_FULLSCREEN
+    WINDOW_GUI_EXPANDED
+    WINDOW_GUI_NORMAL
+    WINDOW_KEEPRATIO
+    WINDOW_NORMAL
+    WINDOW_OPENGL
+    '''
+    cv2.namedWindow(title, cv2.WINDOW_NORMAL)
+    cv2.imshow(title, image)
+    cv2.waitKey(0)
+    cv2.destroyAllWindows()
+    return True
