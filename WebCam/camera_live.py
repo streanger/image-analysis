32c5c792cff955f5a6ec6d9f54baf43e8247d6c7
streanger <pslisz@o2.pl>
2019-02-25 23:27:50 +0100

    Add files via upload

--- WebCam/camera_live.py
+++ WebCam/camera_live.py
@@ -1,8 +1,10 @@
 import os
 import sys
+import time
 import numpy as np
 import cv2
 
+import image_rgb_roll
 
 def script_path():
     '''set current path to script_path'''
@@ -33,13 +35,18 @@ def draw_over(img):
 def draw_contours(image):
     ''' find contours and draw over '''
     gray = cv2.cvtColor(image, cv2.COLOR_BGR2GRAY)
-    ret, thresh = cv2.threshold(gray, 50, 255, cv2.THRESH_BINARY)
+    ret, thresh = cv2.threshold(gray, 10, 255, cv2.THRESH_BINARY)           # 30 is ok
     im2, contours, hierarchy = cv2.findContours(thresh, cv2.RETR_TREE, cv2.CHAIN_APPROX_SIMPLE)
     
     # sort contours
     contoursSorted = sorted(contours, key=lambda x: cv2.contourArea(x), reverse=True)
-    
-    for key, contour in enumerate(contoursSorted[:50]):
+    loopRange = 50
+    if len(contoursSorted) < 50:
+        loopRange = len(contoursSorted)
+    if not len(contoursSorted):
+        raise ValueError('A very specific bad thing happened.')
+    print(len(contoursSorted))
+    for key, contour in enumerate(contoursSorted[:loopRange]):
         # color_contours = (150, 255, 150)            # green - color for contours
         color_contours = (50, 255, 50)            # green - color for contours
         cv2.drawContours(image, contoursSorted, key, color_contours, 1, 4, hierarchy)
@@ -62,13 +69,28 @@ def main(args):
         # backtorgb = cv2.cvtColor(gray,cv2.COLOR_GRAY2RGB) # is the correct syntax.
         
         # ********************** enlarge frame **********************
-        size = 2
+        size = 1
         out = np.repeat(np.repeat(frame, size, axis=0), size, axis=1)
+        b_channel, g_channel, r_channel = cv2.split(out)
+        out = cv2.merge((g_channel, r_channel, b_channel))
+        
+        
+        # ********************** roll image **********************
+        # seconds = int(time.strftime("%S"))
+        # now = round(time.time()%1*1000)/200
+        # out = image_rgb_roll.rgb_roll(out, now, 10)
+        # out = image_rgb_roll.rgb_roll(out, now, now)
+        out = image_rgb_roll.rgb_roll(out, 45, 3)
         
         
         # ********************** draw contours **********************
-        out = draw_contours(out)
-        
+        try:
+            # pass
+            out = draw_contours(out)
+        except:
+            print("fail to draw conturs")
+            # thats why we need to extract contours first and check if they exist -> this is very todo
+            continue
         
         # Display the resulting frame
         cv2.imshow('out', out)
